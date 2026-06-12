2fba3e81bdf243d185b104afcd2c68ae05d6acd5
Hiuri Selzler <hiuriselzler0@gmail.com>
2026-06-12 14:58:34 -0300

    Add Navbar function

--- projetoEcommerce/src/App.jsx
+++ projetoEcommerce/src/App.jsx
@@ -3,10 +3,12 @@ import './App.css'
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import Checkout from "./pages/Checkout";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className = "app">
+      <Navbar />
       <Routes>
         <Route path = "/"  element= {<Home />} />
         <Route path = "/auth" element= {<Auth />} />
