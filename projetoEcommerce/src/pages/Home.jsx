11a8bbbd645ab8a7c5d00dc052bd5faa6a847506
Hiuri Selzler <hiuriselzler0@gmail.com>
2026-06-12 16:16:42 -0300

    re-organization of Product card

--- projetoEcommerce/src/pages/Home.jsx
+++ projetoEcommerce/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import ProductCard from "../components/ProductCard";
 import { getProducts } from "../data/products";
 import { Link } from "react-router-dom";
 
@@ -18,24 +19,9 @@ export default function Home(){
             <h2 className="page-title">
                 Our products
             </h2>
-            <div className="products-grid">
+            <div className="product-grid">
                 {products.map((product)=>(
-                    <div className="product-card" key={product.id}>
-                        <img src={product.image}  className="product-card-image" />
-                        <div className="product-card-content">
-                            <h3 className="product-card-name">
-                                {product.name}
-                            </h3>
-                            <p className="product-card-price">
-                                ${product.price}
-                            </p>
-                            <div className="product-card-actions">
-                                <Link className="btn btn-secundary">View Details</Link>
-                                <button className="btn btn-primary">Add to cart</button>
-                            </div>
-
-                        </div>
-                    </div>
+                    <ProductCard product ={product} key={product.id} />      
                 ))}
             </div>
         </div>
