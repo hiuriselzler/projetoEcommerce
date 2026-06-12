95ee9e0e4e0c3ce026845a67e323aa240f87c561
Hiuri Selzler <hiuriselzler0@gmail.com>
2026-06-12 15:04:36 -0300

    add new div to function Home

--- projetoEcommerce/src/pages/Home.jsx
+++ projetoEcommerce/src/pages/Home.jsx
@@ -2,5 +2,19 @@
 
 
 export default function Home(){
-    return <div>Home Page</div>
+    return(
+     <div className="page">
+        <div className="home-hero">
+            <h1 className="home-title">
+                Welcome to ShopHub
+            </h1>
+            <p>
+                Discover amazing products at great prices
+            </p>
+        </div>
+        <div className="container">
+            <h2 className="page-title">Our products</h2>
+        </div>
+    </div>
+    );
 }
